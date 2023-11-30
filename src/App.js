bb7448945c25c623e02aac4f1b46c7397fbe860e
Dinuka Amarasinghe <dinukaamarasinghe817@gmail.com>
2023-11-30 12:45:54 +0530

    added an un authorized path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Routes} from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import Auth from './Auth';
+import Next from './Next';
 
 function App() {
   return (
