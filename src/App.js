2cdc1147815e0c6619e67b8cba1e308c2bc6f275
Dinuka Amarasinghe <dinukaamarasinghe817@gmail.com>
2023-11-30 14:35:21 +0530

    added simpler path

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './Home';
 import Auth from './Auth';
 import Next from './Next';
+import NewComp from './NewComp';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/auth" element={<Auth/>}/>
+        <Route path="/newcomponent" element={<NewComp/>}/>
         <Route path="/next" element={<Next/>}/>
     </Routes>
   );
---
+++ src/NewComp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewComp = () => {
+  return (
+    <div>Paragraph</div>
+  )
+}
+
+export default NewComp
