e0805ed03cd7cc4d12eb5142f5fb057428eb456c
Dinuka Amarasinghe <dinukaamarasinghe817@gmail.com>
2023-11-30 14:54:24 +0530

    added auth content to new component

--- src/NewComp.js
+++ src/NewComp.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { useAuthContext } from "@asgardeo/auth-react";
 
 const NewComp = () => {
-  return (
-    <div>Paragraph</div>
-  )
+    const { state: { isAuthenticated }, signOut } = useAuthContext();
+
+    if(isAuthenticated){
+      return (
+        <>
+        <div className='content'>Authenticated</div>
+        <button onClick={() => signOut()}>SignIn</button>
+        </>
+      );
+    }else{
+      return (
+        <div className='content'>Not Authenticated</div>
+      );
+    }
 }
 
 export default NewComp
