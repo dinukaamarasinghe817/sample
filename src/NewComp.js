31caa873715f06d33139dd049a77c662215d4dbb
Dinuka Amarasinghe <dinukaamarasinghe817@gmail.com>
2023-11-30 18:06:08 +0530

    add configuration to index.js

--- src/NewComp.js
+++ src/NewComp.js
@@ -2,20 +2,28 @@ import React from 'react';
 import { useAuthContext } from "@asgardeo/auth-react";
 
 const NewComp = () => {
-    const { state: { isAuthenticated }, signOut } = useAuthContext();
+  const { getDecodedIDToken, signOut } = useAuthContext();
+  // console.log(getDecodedIDToken);
 
-    if(isAuthenticated){
-      return (
-        <>
-        <div className='content'>Authenticated</div>
-        <button onClick={() => signOut()}>SignIn</button>
-        </>
-      );
-    }else{
-      return (
-        <div className='content'>Not Authenticated</div>
-      );
-    }
+  async function getToken(){
+    const decodedIDToken = await getDecodedIDToken();
+    console.log(decodedIDToken);
+  }
+
+  getToken();
+
+  // if(getDecodedIDToken){
+  //   return (
+  //     <>
+  //     <div className='content'>Authenticated</div>
+  //     <button onClick={() => signOut()}>SignIn</button>
+  //     </>
+  //   );
+  // }else{
+  //   return (
+  //     <div className='content'>Not Authenticated</div>
+  //   );
+  // }
 }
 
 export default NewComp
