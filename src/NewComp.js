8993d8a8e620bf6f56925c7d78548f86b6bbbac4
Dinuka Amarasinghe <dinukaamarasinghe817@gmail.com>
2023-11-30 22:24:24 +0530

    add configuration to index.js

--- src/NewComp.js
+++ src/NewComp.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { useAuthContext } from "@asgardeo/auth-react";
 
 const NewComp = () => {
-  const { state, getDecodedIDToken, signOut } = useAuthContext();
+  const { state, getDecodedIDToken, getBasicUserInfo, signOut } = useAuthContext();
   // console.log(getDecodedIDToken);
 
   async function getToken(){
     getDecodedIDToken().then((res) => {
       console.log(res);
       console.log("here is the state variable");
+      console.log(state);
+    })
+    
+    getBasicUserInfo().then((basicUserDetails) => {
+      console.log(basicUserDetails);
       console.log(state);
     })
   }
