c8933c676d826675bf4ee91a5d2169829eb2feb5
Dinuka Amarasinghe <dinukaamarasinghe817@gmail.com>
2023-12-01 13:39:30 +0530

    added choreo configs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from "@asgardeo/auth-react";
-// import { TokenExchangePlugin } from "@asgardeo/token-exchange-plugin";
+import { TokenExchangePlugin } from "@asgardeo/token-exchange-plugin";
 import authconfig from './config.json';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,7 +12,7 @@ root.render(
     <BrowserRouter>
       <AuthProvider 
           config={{...authconfig}}
-          // plugin={ TokenExchangePlugin.getInstance() }
+          plugin={ TokenExchangePlugin.getInstance() }
       >
         <App />
       </AuthProvider>
